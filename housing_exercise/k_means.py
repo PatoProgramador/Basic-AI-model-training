74f39ef25de75d16fb5a9ebf4058997064e501dc
PatoProgramador <skpates18@gmail.com>
2024-05-02 20:09:29 -0500

    K_means panda columns

--- housing_exercise/k_means.py
+++ housing_exercise/k_means.py
@@ -1,5 +1,19 @@
 import pandas as pd
+import seaborn as sb
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 datos = pd.read_csv("housing.csv")
 
-print(datos.head())
+X = datos.loc[:,["latitude", "longitude", "median_income"]]
+
+model = KMeans(n_clusters=6)
+predict = model.fit_predict(X)
+
+predict.shape
+
+X["segmento_economico"] = predict
+
+# sb.scatterplot(x="longitude", y="latitude", data=X, hue="segmento_economico", palette="coolwarm", size="segmento_economico", sizes=(20,50))
+# sb.countplot(x="segmento_economico", data=X)
+print(X.groupby(["segmento_economico"])["median_income"].mean())
