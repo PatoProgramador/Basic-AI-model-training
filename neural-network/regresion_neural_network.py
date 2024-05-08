4e7825ac58d9936e421342c3fc566a41d3025b04
PatoProgramador <skpates18@gmail.com>
2024-05-07 19:41:42 -0500

    Regresion neural network model

---
+++ neural-network/regresion_neural_network.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+datos = pd.read_csv('celsius.csv')
+
+X = datos["celsius"].values
+y = datos["fahrenheit"].values
+
+model = tf.keras.sequential([
+    tf.keras.layers.Input(shape=(1,)), # La entrada, los datos celsius: 1 solo dato
+    tf.keras.layer.Dense(units=1), # La salida, es un solo dato, los grados fahrenheit
+])
+
+model.compile(
+    optimizer= tf.keras.optimizers.Adam(0.1),
+    loss="mean_squared_error"
+)
+
+training = model.fit(X, y, epochs=1000)
+
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de perdida")
+plt.plot(training.history["loss"])
+
+# prediccion
+c = 18
+predict = model.predict([c])
+print(f"Prediccion: {c} son {predict[0]} fahrenheit")
+
+print(model.layers[0].get_weights())
